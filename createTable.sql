abe94f81568254c497515b3c9daef3c741d3c36f
chaxyz <105903353+chaxyz@users.noreply.github.com>
2024-05-07 14:10:14 +0700

    Update createTable.sql

--- createTable.sql
+++ createTable.sql
@@ -1,16 +1,15 @@
 create database integreated;
 use integreated;
 CREATE TABLE `tasks` (
-    `taskId` INT NOT NULL AUTO_INCREMENT,
-    `taskTitle` VARCHAR(100) NOT NULL,
-    `taskDescription` VARCHAR(500) DEFAULT NULL,
-    `taskAssignees` VARCHAR(30) DEFAULT NULL,
-    `taskStatus` ENUM('NO_STATUS', 'TO_DO', 'DOING', 'DONE') NOT NULL DEFAULT 'NO_STATUS',
-    `createdOn` DATETIME DEFAULT CURRENT_TIMESTAMP,
-    `updatedOn` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`taskId`),
-    UNIQUE KEY `taskId_UNIQUE` (`taskId`)
-)  ENGINE=INNODB DEFAULT CHARSET=UTF8MB4 COLLATE = UTF8MB4_0900_AI_CI;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `taskTitle` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `taskDescription` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `taskAssignees` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `taskStatus` enum('NO_STATUS','TO_DO','DOING','DONE') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT 'NO_STATUS',
+  `createdOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 SET GLOBAL time_zone = '+00:00';
 
