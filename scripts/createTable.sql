9124932f19bf64866550319fc0baf7058094c0d9
chaxyz <chaiithep2546@gmail.com>
2024-05-14 13:14:38 +0700

    add taskv2

--- scripts/createTable.sql
+++ scripts/createTable.sql
@@ -11,5 +11,25 @@ CREATE TABLE `tasks` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+CREATE TABLE `tasksv2` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `taskTitle` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `taskDescription` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `taskAssignees` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `taskStatus` int DEFAULT NULL,
+  `createdOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedOn` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `FK_statusId` (`taskStatus`),
+  CONSTRAINT `FK_statusId` FOREIGN KEY (`taskStatus`) REFERENCES `status` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `status` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `statusName` varchar(50) NOT NULL,
+  `statusDescription` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `statusName` (`statusName`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 SET GLOBAL time_zone = '+00:00';
 
--- scripts/insertData.sql
+++ scripts/insertData.sql
@@ -12,3 +12,23 @@ VALUES ('ดาต้าเบส', 'ສ້າງຖານຂໍ້ມູນ', '
 
 INSERT INTO tasks (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn) 
 VALUES ('_Infrastructure_', '_Setup containers_', 'ไก่งวง กับ เพนกวิน' , 'Done' , '2024-04-22 09:15:00', '2024-04-22 10:00:00');
+
+INSERT INTO `status` (`statusName`,statusDescription) VALUES
+('No Status', 'The default status'),
+('To Do',null),
+('Doing','Being worked on'),
+('Done','Finished');
+
+INSERT INTO tasksv2 (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn) 
+VALUES ('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 
+'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 
+'Assignees1Assignees2Assignees3', 1 , '2024-04-22 09:00:00', '2024-04-22 09:00:00');
+
+INSERT INTO tasksv2 (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn) 
+VALUES ('Repository', null , null , 2 , '2024-04-22 09:05:00', '2024-04-22 14:00:00');
+
+INSERT INTO tasksv2 (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn) 
+VALUES ('ดาต้าเบส', 'ສ້າງຖານຂໍ້ມູນ', 'あなた、彼、彼女 (私ではありません)' , 3 , '2024-04-22 09:10:00', '2024-04-25 00:00:00');
+
+INSERT INTO tasksv2 (taskTitle, taskDescription, taskAssignees, taskStatus, createdOn, updatedOn) 
+VALUES ('_Infrastructure_', '_Setup containers_', 'ไก่งวง กับ เพนกวิน' , 4 , '2024-04-22 09:15:00', '2024-04-22 10:00:00');
